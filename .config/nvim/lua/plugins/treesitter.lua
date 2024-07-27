a09c473237835d4ccf55dd3e711bb87e1d00ac08
Golf0ned <benjaminye.email@gmail.com>
2024-07-26 19:13:57 -0700

    forgot a comma. uniformity good

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,31 +1,33 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = {
-                "bash",
-                "c",
-                "cpp",
-                "css",
-                "html",
-                "javascript",
-                "llvm",
-                "lua",
-                "markdown",
-                "python",
-                "query",
-                "rust",
-                "toml",
-                "vim",
-                "vimdoc",
-            },
-            sync_install = false,
-            auto_install = true,
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-        })
-    end
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = {
+                    "bash",
+                    "c",
+                    "cpp",
+                    "css",
+                    "html",
+                    "javascript",
+                    "llvm",
+                    "lua",
+                    "markdown",
+                    "python",
+                    "query",
+                    "rust",
+                    "toml",
+                    "vim",
+                    "vimdoc",
+                },
+                sync_install = false,
+                auto_install = true,
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false,
+                },
+            })
+        end,
+    },
 }
