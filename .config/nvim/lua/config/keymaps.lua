9adcab83d20f781b5ebaf297527ce04a7878d77f
Golf0ned <benjaminye.email@gmail.com>
2024-07-26 17:11:39 -0700

    more work on nvim

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,18 @@
+-- back from editor
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- J/K visual moving
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- search terms stay centered
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- system clipboard
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- delete to void
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
