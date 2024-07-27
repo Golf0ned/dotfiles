5b9354626f23797d5a32ee5ff41ad2eff75cad49
Golf0ned <benjaminye.email@gmail.com>
2024-07-27 00:28:59 -0700

    vim-airline

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -8,5 +8,17 @@ return {
             vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
             vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
         end
-    }
+    },
+    {
+        "vim-airline/vim-airline",
+        dependencies = {"vim-airline/vim-airline-themes"},
+        config = function()
+            vim.cmd[[
+                let g:airline#extensions#tabline#enabled = 1
+                let g:airline#extensions#tabline#formatter = 'default'
+                let g:airline_theme = 'bubblegum'
+                let g:airline_powerline_fonts = 1
+            ]]
+        end
+    },
 }
