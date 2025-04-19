c57b62f00870721d30301b22b9d405318399f50f
Golf0ned <benjaminye.email@gmail.com>
2025-04-19 15:44:42 -0500

    bye bye wezterm ligatures

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,8 +3,11 @@ local wezterm = require "wezterm"
 local config = wezterm.config_builder()
 
 config.color_scheme = "Catppuccin Mocha"
+
 -- config.font = wezterm.font("UbuntuMono Nerd Font")
 -- config.font_size = 14
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
+
 config.initial_cols = 150
 config.initial_rows = 50
 
