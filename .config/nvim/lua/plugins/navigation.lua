d8a247720f05e371dedad524fa1e3f424e244adb
Golf0ned <benjaminye.email@gmail.com>
2025-04-19 21:42:10 -0500

    harpoon h overrided h

--- .config/nvim/lua/plugins/navigation.lua
+++ .config/nvim/lua/plugins/navigation.lua
@@ -23,12 +23,12 @@ return {
         config = function()
             local harpoon = require('harpoon')
             harpoon:setup()
-            vim.keymap.set("n", "hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-            vim.keymap.set("n", "ha", function() harpoon:list():add() end)
-            vim.keymap.set("n", "h1", function() harpoon:list():select(1) end)
-            vim.keymap.set("n", "h2", function() harpoon:list():select(2) end)
-            vim.keymap.set("n", "h3", function() harpoon:list():select(3) end)
-            vim.keymap.set("n", "h4", function() harpoon:list():select(4) end)
+            vim.keymap.set("n", "<leader>hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+            vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end)
+            vim.keymap.set("n", "<leader>h1", function() harpoon:list():select(1) end)
+            vim.keymap.set("n", "<leader>h2", function() harpoon:list():select(2) end)
+            vim.keymap.set("n", "<leader>h3", function() harpoon:list():select(3) end)
+            vim.keymap.set("n", "<leader>h4", function() harpoon:list():select(4) end)
         end
     },
 }
