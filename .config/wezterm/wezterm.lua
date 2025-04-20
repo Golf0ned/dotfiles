8939cdb3c6e96ad306f37680f168adcad3108587
Golf0ned <benjaminye.email@gmail.com>
2025-04-20 02:21:02 -0500

    update tmux and wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,15 +4,18 @@ local config = wezterm.config_builder()
 
 config.color_scheme = "Catppuccin Mocha"
 
--- config.font = wezterm.font("UbuntuMono Nerd Font")
--- config.font_size = 14
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 
 config.initial_cols = 150
-config.initial_rows = 50
+config.initial_rows = 40
+config.window_padding = {
+    left = "0.5cell",
+    right = "0.5cell",
+    top = "0.25cell",
+    bottom = "0.25cell",
+}
 
 config.audible_bell = "Disabled"
 
-config.default_domain = "WSL:Ubuntu"
 
 return config
