010b0e50029817db62d6735f41645f8242b9c35e
Golf0ned <benjaminye.email@gmail.com>
2025-04-21 04:54:27 -0500

    safer telescope stuff

--- .config/nvim/lua/plugins/navigation.lua
+++ .config/nvim/lua/plugins/navigation.lua
@@ -2,19 +2,24 @@ return {
     {
         "nvim-telescope/telescope.nvim",
         tag = "0.1.8",
-        dependencies = { "nvim-lua/plenary.nvim" },
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            {
+                "nvim-telescope/telescope-fzf-native.nvim",
+                build = "make",
+                cond = function()
+                    return vim.fn.executable("make") == 1
+                end,
+            }
+        },
         config = function()
             local builtin = require("telescope.builtin")
             vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
             vim.keymap.set("n", "<leader>fg", function()
                 builtin.grep_string({ search = vim.fn.input("Grep > ") })
             end)
-            require("telescope").load_extension("fzf")
+            pcall(require("telescope").load_extension("fzf"))
         end
-    },
-    {
-        "nvim-telescope/telescope-fzf-native.nvim",
-        build = "make",
     },
     {
         "ThePrimeagen/harpoon",
