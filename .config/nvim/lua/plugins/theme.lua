f85009776a8530d3b194c6a65323bc02cce988ae
Golf0ned <benjaminye.email@gmail.com>
2025-04-28 13:14:07 -0500

    i figured out which theme i DONT like

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -15,15 +15,8 @@ return {
             -- vim.cmd.colorscheme "catppuccin-mocha"
             -- vim.cmd.colorscheme "catppuccin-macchiato"
             -- vim.cmd.colorscheme "catppuccin-frappe"
+
             -- vim.cmd.colorscheme "catppuccin-latte"
-        end
-    },
-    {
-        'b0o/lavi.nvim',
-        dependencies = { 'rktjmp/lush.nvim' },
-        priority = 1000,
-        config = function()
-            -- vim.cmd.colorscheme "lavi"
         end
     },
     {
@@ -31,22 +24,25 @@ return {
         priority = 1000,
         opts = {},
         config = function()
-            -- vim.cmd.colorscheme "tokyonight-day"
+            -- vim.cmd.colorscheme "tokyonight-moon"
             -- vim.cmd.colorscheme "tokyonight-night"
             -- vim.cmd.colorscheme "tokyonight-storm"
+
+            -- vim.cmd.colorscheme "tokyonight-day"
         end
     },
     {
         "EdenEast/nightfox.nvim",
         priority = 1000,
         config = function()
-            -- vim.cmd.colorscheme "carbonfox"
-            -- vim.cmd.colorscheme "dawnfox"
-            -- vim.cmd.colorscheme "dayfox"
+            vim.cmd.colorscheme "carbonfox"
             -- vim.cmd.colorscheme "duskfox"
             -- vim.cmd.colorscheme "nightfox"
             -- vim.cmd.colorscheme "nordfox"
-            vim.cmd.colorscheme "terafox"
+            -- vim.cmd.colorscheme "terafox"
+
+            -- vim.cmd.colorscheme "dawnfox"
+            -- vim.cmd.colorscheme "dayfox"
         end
     },
 }
